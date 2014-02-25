8bd59a26db0b507b072017e623fd5b3a21042e57
Amina <Amina@192.168.1.26>
2014-02-25 23:47:39 +0100

    Rajout des profils

---
+++ gcm/gcm-dao/src/main/java/fr/gcm/dao/IProfileRepository.java
@@ -0,0 +1,16 @@
+package fr.gcm.dao;
+
+import fr.gcm.model.Profiles;
+
+/**
+ * 
+ * @author Amina
+ *		  
+ *		Interface profiles
+ *			
+ */
+
+public interface IProfileRepository {
+	
+	void addProfile (Profiles profile);
+}
---
+++ gcm/gcm-dao/src/main/java/fr/gcm/dao/impl/ProfileRepositoryImpl.java
@@ -0,0 +1,22 @@
+package fr.gcm.dao.impl;
+
+import fr.gcm.dao.IProfileRepository;
+import fr.gcm.model.Profiles;
+
+/**
+ * 
+ * @author Amina
+ * 
+ * 			Implementation des services profiles 
+ *
+ */
+public class ProfileRepositoryImpl implements IProfileRepository {
+	
+	
+
+	@Override
+	public void addProfile(Profiles profile) {
+		
+	}
+
+}
